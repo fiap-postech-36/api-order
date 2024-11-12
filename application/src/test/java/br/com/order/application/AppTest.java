316ed1d5e54a21eb69ab0cd4e5aea8c2e17dc6c1
leand <leandroibraimads@gmail.com>
2024-11-12 01:03:34 -0300

    test: impl teste unitario app

---
+++ application/src/test/java/br/com/order/application/AppTest.java
@@ -0,0 +1,13 @@
+package br.com.order.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+
+    @Test
+    void contextLoads() {
+        // Este teste verifica se o contexto da aplicação Spring Boot é carregado corretamente.
+    }
+}
