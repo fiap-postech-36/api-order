48be063adf761897bbc30d61848cf08cedafd161
leand <leandroibraimads@gmail.com>
2024-11-11 21:46:55 -0300

    impl ms order

---
+++ application/src/test/java/br/com/order/application/exception/CustomerAlreadyExistsExceptionTest.java
@@ -0,0 +1,28 @@
+package br.com.order.application.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CustomerAlreadyExistsExceptionTest {
+
+    @Test
+    void testExceptionMessage() {
+        String errorMessage = "Customer already exists!";
+        CustomerAlreadyExistsException exception = new CustomerAlreadyExistsException(errorMessage);
+
+        assertEquals(errorMessage, exception.getMessage());
+    }
+
+    @Test
+    void testExceptionThrown() {
+        String errorMessage = "Customer already exists!";
+
+        Exception exception = assertThrows(CustomerAlreadyExistsException.class, () -> {
+            throw new CustomerAlreadyExistsException(errorMessage);
+        });
+
+        assertEquals(errorMessage, exception.getMessage());
+    }
+}
